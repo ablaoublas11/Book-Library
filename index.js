b1ce82a17b1c8bf18dd4261b8af7cf701fe129ca
ablaoublas11 <xristosxalkidis.exnet@gmail.com>
2026-06-17 23:56:45 +0300

    A SOLID BOOK LIBRARY APPLICATION This is a simple book library application built using JavaScript, HTML, and CSS. It allows

---
+++ index.js
@@ -0,0 +1,97 @@
+//αυτήν είναι η λίτα στην οποία αποθηκεύουμε τα αντικέιμενα Book
+const bookList = [];
+
+//Αυτός είναι ο δομητής της Book
+function Book(name, author, numOfPage) {
+  this.id = crypto.randomUUID();
+  this.name = name;
+  this.author = author;
+  this.numOfPages = numOfPage;
+  this.hasRead = false;
+}
+
+//μέθοδος για την προσθήκη αντικειμένων στην λίστα
+function addBookToLibrary(name, author, numOfPage) {
+  const book = new Book(name, author, numOfPage);
+  bookList.push(book);
+  //alert("Added succesfuly");
+}
+
+//από αυτό το κομμάτι του κώδικα μπορούμε να χειριζόμαστε το DOM και να προσθέτουμε λειτουργηκότητα σε buttons κλπ
+const app = {
+  data: bookList,
+  init() {
+    document.querySelector("#addBook").addEventListener("click", () => {
+      const name = document.querySelector("#name").value;
+      const author = document.querySelector("#author").value;
+      const numOfPages = Number(document.querySelector("#numOfPages").value);
+
+      if (!name || !author || !numOfPages) {
+        alert("Please enter name of the book or author's name or book's pages");
+        return;
+      }
+
+      addBookToLibrary(name, author, numOfPages);
+      this.render();
+    });
+    this.render();
+  },
+  render() {
+    const showingContainer = document.querySelector("#bookShowingArea");
+    showingContainer.innerHTML = "";
+
+    if (this.data.length === 0) {
+      const empty = document.createElement("span");
+      empty.id = "empty";
+      empty.textContent = "Empty book list";
+      showingContainer.append(empty);
+      return;
+    }
+
+    this.data.map((item) => {
+      const div = document.createElement("div");
+      div.className = "book-item";
+
+      const removeBtn = document.createElement("button");
+      removeBtn.textContent = "X";
+      removeBtn.onclick = () => app.remove(item.id);
+
+      const toggleBtn = document.createElement("button");
+      toggleBtn.textContent = "Have Read";
+      toggleBtn.onclick = () => app.toogleRead(item.id);
+
+      div.append(
+        Object.assign(document.createElement("span"), {
+          textContent: item.name,
+        }),
+        " ",
+        Object.assign(document.createElement("span"), {
+          textContent: item.author,
+        }),
+        " ",
+        Object.assign(document.createElement("span"), {
+          textContent: item.numOfPages,
+        }),
+        " ",
+        Object.assign(document.createElement("span"), {
+          textContent: item.hasRead,
+        }),
+        removeBtn,
+        toggleBtn,
+      );
+
+      showingContainer.append(div);
+    });
+  },
+  remove(id) {
+    this.data = this.data.filter((i) => i.id !== id);
+    this.render();
+  },
+  toogleRead(id) {
+    this.data = this.data.map((item) =>
+      item.id === id ? { ...item, hasRead: !item.hasRead } : item,
+    );
+    this.render();
+  },
+};
+app.init();
