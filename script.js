7e30b0cb963d2fdf3af6d7f8602e39eb8ed53eaa
ablaoublas11 <50484255+ablaoublas11@users.noreply.github.com>
2026-07-14 08:31:40 +0000

    Refactor code using ES6 features and improve readability.

--- script.js
+++ script.js
@@ -14,13 +14,104 @@ class Library {
         this.data = [];
     }
 
-    init(){}
+    init(){
+      const addButton = document.querySelector(".add-book");
+      addButton.addEventListener("click", () => {
+        document.querySelector(".insertBook").classList.toggle("showing");
+        addButton.textContent = addButton.textContent === "Add Book" ? "Close" : "Add Book";
+      });
 
-    render(){}
+      const name = document.querySelector("#name");
+      const author = document.querySelector("#author");
+      const numOfPages = document.querySelector("#numOfPages");
+      document.querySelector("#addBook").addEventListener("click", () => {
+       if (!name.value || !author.value || !Number(numOfPages.value)) {
+         alert("Please enter name of the book or author's name or book's pages");
+         return;
+       }
 
-    remove(){}
+       this.addBookToLibrary(name.value, author.value, Number(numOfPages.value));
 
-    toggleRead(){}
+       name.value = "";
+       author.value = "";
+       numOfPages.value = "";
 
-    addBookToLibrary(){}
+       this.render();
+     });
+     this.render();
+    }
+
+    //μέθοδος για την εμφάνιση των βιβλίων στην λίστα
+    render(){
+      const showingContainer = document.querySelector("#bookShowingArea");
+      showingContainer.innerHTML = "";
+
+      if (this.data.length === 0) {
+        const empty = document.createElement("span");
+        empty.id = "empty";
+        empty.textContent = "Empty book list";
+        showingContainer.append(empty);
+        return;
+      }
+
+      this.data.forEach((item) => {
+        const div = document.createElement("div");
+        div.className = "book-item";
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "X";
+        removeBtn.onclick = () => this.remove(item.id);
+
+        const toggleBtn = document.createElement("button");
+        toggleBtn.textContent = "Have Read";
+        toggleBtn.onclick = () => this.toggleRead(item.id);
+
+        div.append(
+          Object.assign(document.createElement("span"), {
+            innerHTML: `Title: <strong>${item.name}</strong>`,
+          }),
+          " ",
+          Object.assign(document.createElement("span"), {
+            innerHTML: `Author: <strong>${item.author}</strong>`,
+          }),
+          " ",
+          Object.assign(document.createElement("span"), {
+            innerHTML: `Pages: <strong>${item.numOfPages}</strong>`,
+          }),
+          " ",
+          Object.assign(document.createElement("span"), {
+            innerHTML: `Read: <strong>${item.hasRead ? "Yes" : "No"}</strong>`,
+          }),
+          removeBtn,
+          toggleBtn,
+        );
+
+        showingContainer.append(div);
+      });
+    }
+
+    remove(id){
+      const index = this.data.findIndex(i => i.id === id);
+      if(index !== -1){
+        this.data.splice(index,1);
+      }
+      this.render();
+    }
+
+    toggleRead(id){
+      const book = this.data.find(i => i.id === id);
+      if(book){
+        book.hasRead = !book.hasRead;
+      }
+      this.render();
+    }
+
+    //Μέθοδος για την προσθήκη ενός βιβλίου στην λίστα μας
+    addBookToLibrary(name, author, numOfPage){
+      const book = new Book(name, author, numOfPage);
+      this.data.push(book);
+    }
 }
+
+const library = new Library();
+library.init();
